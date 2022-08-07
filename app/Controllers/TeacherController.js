601303bb730b3379460350ab8bd0529c8b6395c3
budianonym <budianonym@gmail.com>
2022-08-07 10:38:07 +0700

    add crud pattern

--- app/Controllers/TeacherController.js
+++ app/Controllers/TeacherController.js
@@ -1,49 +1,30 @@
-import {
-  DB, Log, Mail, Queue, Fetch,
-  FilterSearch,
-} from '@averoa/utilities';
-import { Teacher } from '@averoa/models';
-import moment from 'moment';
+import { Crud } from '@averoa/utilities';
 
 class TeacherController {
   async create(req, res) {
-    const data = await Teacher.query().insert({ ...req.dataReq });
-    res.send('data');
+    const data = await Crud.create('Teacher', req);
+    res.send(data);
   }
 
   async findAll(req, res) {
-    const data = await Teacher.filterSearch.call(Teacher, req);
-    // const data = await FilterSearch.knex(DB('teacher'), req);
+    const data = await Crud.findAll('Teacher', req);
     res.send(data);
   }
 
   async findOne(req, res) {
-    const data = await Teacher.query().withGraphFetched('rooms').findById(req.params.id);
-    res.json(data);
+    const data = await Crud.findOne('Teacher', req, { relations: 'rooms' });
+    res.send(data);
   }
 
   async update(req, res) {
-    const data = await Teacher.query()
-      .findById(req.params.id)
-      .patch({ ...req.dataReq });
-    res.send(`update ${req.params.id}`);
+    const data = await Crud.update('Teacher', req);
+    res.send(data);
   }
 
   async delete(req, res) {
-    const data = await Teacher.query()
-      .findById(req.params.id)
-      .patch({ deleted_at: moment().format('YYYY-MM-DD, hh:mm:ss') });
-    res.send(`delete ${req.params.id}`);
+    const data = await Crud.delete('Teacher', req, { update: { deleted_by: 8 } });
+    res.send(data);
   }
 }
 
 export default new TeacherController;
-
-// let data = Teacher.query();
-// if (Object.keys(req.query).length !== 0) {
-//   data = data.where(req.query);
-// }
-
-// if (req.search) {
-//   data = Teacher.search.apply(Teacher, [data, req.search]);
-// }
