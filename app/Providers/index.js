b434461b3004e13c92a10a1af19ba94f23e3a928
budianonym <budianonym@gmail.com>
2022-08-17 20:14:22 +0700

    add provider upload

--- app/Providers/index.js
+++ app/Providers/index.js
@@ -1,3 +1,4 @@
 export { default as AppProvider } from './AppProvider.js';
 export { default as ViewProvider } from './ViewProvider.js';
 export { default as MiddlewareProvider } from './MiddlewareProvider.js';
+export { default as UploadProvider } from './UploadProvider.js';
