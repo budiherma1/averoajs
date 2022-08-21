3e3e92df08e1f71550e4f990ea1026ac70dfb8a0
budianonym <budianonym@gmail.com>
2022-08-21 07:37:28 +0700

    adjust default route

--- routes/api.js
+++ routes/api.js
@@ -6,11 +6,11 @@ router.get('/', 'SampleController@sampleMethod');
 router.post('/users', 'SampleController@sampleMethodModelPost');
 
 // this is prebuild route pattern of averoajs
-router.set('Teacher', 'teachers', () => { // first parameter is the models name, then the second is the prefix path
+router.crud('Teacher', 'teachers', () => { // first parameter is the models name, then the second is the prefix path
   router.get('/', 'TeacherController@findAll');
   router.get('/:id', 'TeacherController@findOne');
   router.post('/', 'TeacherController@create'); // use router.postForm if the request use multipart/form
-  router.patch('/:id', 'TeacherController@update'); // use router.patchForm if the request use multipart/form
+  router.put('/:id', 'TeacherController@update'); // use router.patchForm if the request use multipart/form
   router.delete('/:id', 'TeacherController@delete');
 });
 
